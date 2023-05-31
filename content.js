b925a6e3c7625e7fc9d3aa62ee350c03757529e1
qnezor <qnezoru@gmail.com>
2023-05-31 21:39:41 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var image = document.createElement('img');
-image.src = browser.runtime.getURL("assets/images/neco-arc-dance.gif");
+image.src = chrome.runtime.getURL("assets/images/neco-arc-dance.gif");
 image.style.height = "200px";
 image.style.width = "auto";
 image.style.position = "fixed";
@@ -8,7 +8,7 @@ image.style.right = "20px";
 image.style.zIndex = "9999";
 
 const music = document.createElement("iframe");
-music.src = browser.runtime.getURL("assets/music/bgm.ogg");
+music.src = chrome.runtime.getURL("assets/music/bgm.ogg");
 music.allow = "autoplay 'src'";
 music.style.display = "none";
 
